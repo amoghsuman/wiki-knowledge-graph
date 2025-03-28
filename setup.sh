9435e8c19ce04521fe884bddd35d2bd673f1487f
Amogh Suman <amoghsuman@gmail.com>
2025-03-28 12:57:56 +0530

    💡 Cleaned up for Streamlit Cloud compatibility

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+echo "Downloading spaCy model..."
 python -m spacy download en_core_web_lg
+
+echo "Installing coreferee separately..."
+pip install coreferee==1.1.3
 python -m coreferee install en
